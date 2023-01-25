bd7c0b766cf17c26a8429ad167676eec2761def9
Mauro Tubiana <mauro.tubiana@gmail.com>
2023-01-25 20:04:58 +0100

    brainfuCk: add error argument to the run function

--- include/brainfuCk.h
+++ include/brainfuCk.h
@@ -1,9 +1,13 @@
 #ifndef BRAINFUCK_H
 #define BRAINFUCK_H
 
+typedef struct {
+    int error_code;
+} bf_error_t ;
+
 typedef struct bf_interpreter_t {
     void *interpreter_p_data;
-    void (* run)(struct bf_interpreter_t *self, char *program);
+    void (* run)(struct bf_interpreter_t *self, char *program, bf_error_t *error);
 } bf_interpreter_t;
 
 #endif
